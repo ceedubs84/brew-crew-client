f28db5859d3cb59f0127048ce83987d3173f3e48
Chris Woodside <christopherwoodside@gmail.com>
2016-12-08 21:04:36 -0600

    Ran bundle and updated Brew controller Show action

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -1,9 +1,9 @@
 class BrewsController < ApplicationController
   def index
-    
+
   end
 
   def show
-
+    @brew = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}").body
   end
 end
