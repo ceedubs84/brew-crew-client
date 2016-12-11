63a9974c860425dfd690e2a6847c8a4f9a51dfe1
Chris Woodside <christopherwoodside@gmail.com>
2016-12-11 10:50:26 -0600

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get '/' => 'brews#index'
   get '/brews' => 'brews#index'
-
+  post '/'
+  get '/brews'
   get '/brews/:id' => 'brews#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
