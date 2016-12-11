5ee8f847df1863191932a517ad02300f525750b1
Chris Woodside <christopherwoodside@gmail.com>
2016-12-11 11:49:06 -0600

    Added New and Edit routes, added Edit and Update actions to Brews controller

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -4,9 +4,39 @@ class BrewsController < ApplicationController
     render 'index.html.erb'
   end
 
+  def create
+    
+  end
+
   def show
     @brew = Unirest.get("http://localhost:3000/api/v1/brews/#{params[:id]}").body
     render 'show.html.erb'
+  end
+
+  def edit
+    @brew = Unirest.get("http://localhost:3000/api/v1/brews/#{params[:id]}")
+    render 'edit.html.erb'
+  end
+
+  def update
+    @brew = Unirest.patch(
+      "http://localhost:3000/api/v1/brews/#{params[:id]}",
+      parameters: {
+        name: params[:name],
+        style: params[:style],
+        hop: params[:hop],
+        yeast: params[:yeast],
+        malts: params[:malts],
+        ibu: params[:ibu],
+        alcohol: params[:alcohol],
+        blg: params[:blg],
+      }
+    ).body
+    redirect_to "/brews/#{@brew['id']}"
+  end
+
+  def destroy
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/brews' => 'brews#index'
+  get '/brews' => 'brews#new'
   post '/brews' => 'brews#create'
   get '/brews/:id' => 'brews#show'
+  get '/brews/:id' => 'brews#edit'
   patch '/brews/:id' => 'brews#update'
   delete '/brews/:id' => 'brews#destroy'
 
