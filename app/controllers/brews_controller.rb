1fba4d482f9eda09794b1496035c07c39deacad5
Chris Woodside <christopherwoodside@gmail.com>
2016-12-11 14:46:26 -0600

    Updated Actions then DRYed the code

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -1,26 +1,51 @@
 class BrewsController < ApplicationController
+  HEADERS = { 'Authorization' => "Token token=#{ENV['API_KEY']}", 'X-User-Email' => ENV['API_EMAIL']}
   def index
-    @brews = Unirest.get("http://localhost:3000/api/v1/brews").body
+    @brews = Unirest.get(
+      "#{ENV['API_BASE_URL']}",
+      headers: HEADERS
+      ).body
     render 'index.html.erb'
   end
 
   def create
-    
+    @brew = Unirest.post(
+      "#{ENV['API_BASE_URL']}/brews",
+      headers: HEADERS,
+      parameters: {
+        name: params[:name],
+        style: params[:style],
+        hop: params[:hop],
+        yeast: params[:yeast],
+        malts: params[:malts],
+        ibu: params[:ibu],
+        alcohol: params[:alcohol],
+        blg: params[:blg]
+      }
+    ).body
+    redirect_to "/brews/#{@brew["id"]}"
   end
 
   def show
-    @brew = Unirest.get("http://localhost:3000/api/v1/brews/#{params[:id]}").body
+    @brew = Unirest.get(
+      "#{ENV['API_BASE_URL']}/brews/#{params[:id]}",
+      headers: HEADERS,
+    ).body
     render 'show.html.erb'
   end
 
   def edit
-    @brew = Unirest.get("http://localhost:3000/api/v1/brews/#{params[:id]}")
+    @brew = Unirest.get(
+      "#{ENV['API_BASE_URL']}/employees/#{params[:id]}",
+      headers: HEADERS,
+    ).body
     render 'edit.html.erb'
   end
 
   def update
     @brew = Unirest.patch(
-      "http://localhost:3000/api/v1/brews/#{params[:id]}",
+      "#{ENV['API_BASE_URL']}/brews/#{params[:id]}",
+      headers: HEADERS,
       parameters: {
         name: params[:name],
         style: params[:style],
@@ -36,7 +61,10 @@ class BrewsController < ApplicationController
   end
 
   def destroy
-    message = Unirest.delete("http://localhost:3000/api/v1/brews/#{params[:id]}").body
+    message = Unirest.delete(
+      "#{ENV['API_BASE_URL']}/brews/#{params[:id]}",
+      headers: HEADERS
+    ).body
     redirect_to "/brews"
   end
 
