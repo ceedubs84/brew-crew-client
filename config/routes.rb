b4739e7818870d02c14a8f2cab55bd022493ed32
Chris Woodside <christopherwoodside@gmail.com>
2016-12-11 11:28:30 -0600

    Added Create, Update, and Destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get '/' => 'brews#index'
-  get '/brews' => 'brews#index'
-  post '/'
-  get '/brews'
-  get '/brews/:id' => 'brews#show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  get '/brews' => 'brews#index'
+  post '/brews' => 'brews#create'
+  get '/brews/:id' => 'brews#show'
+  patch '/brews/:id' => 'brews#update'
+  delete '/brews/:id' => 'brews#destroy'
+
 end
